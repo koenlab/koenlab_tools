dad29f81a55e76a95e98fe140a9ecffdc8fca8fb
jdkoen <koen.joshua@gmail.com>
2019-07-23 15:46:40 -0400

    add figure and table outputs (optional with opts.fig_dir input)

--- MATLAB/eeglab/mark_bad_channels.m
+++ MATLAB/eeglab/mark_bad_channels.m
@@ -70,14 +70,21 @@ if ~isfield(opts, 'badchans')
     opts.badchans = [];
 end
 
+% Handle fig_dir
+if ~isfield(opts,'fig_dir')
+    opts.fig_dir = '';
+end
+
 %% Select channels for analysis (and the field in opts)
 % Determine which channels to process
-if ismember( input_fields, 'chan_inds' ) % Determine by input indicies
+if ismember( 'chan_inds', input_fields ) % Determine by input indicies
     chan_field = 'chan_inds';
     chan_inds = opts.(chan_field);
-else % Determine by type
+elseif ismember( 'chan_type', input_fields ) % Determine by type
     chan_field = 'chan_type';
     chan_inds = find( ismember( {EEG.chanlocs.type}, opts.(chan_field) ) );
+else
+    error('chan_inds or chan_type have not been provided as input. This is required.');
 end
 
 %% Mark bad channels using pop_rejchan
@@ -88,12 +95,12 @@ badchans = struct();
 badchans.given = opts.badchans;
 
 % Run automated bad channel detection w/ probability and spectrum
-[~, badchans.prob] = pop_rejchan(EEG, ...
+[~, badchans.prob_bad, badchans.prob_measure] = pop_rejchan(EEG, ...
     'elec', chan_inds, ...
     'threshold',opts.thresh, ...
     'norm','on', ...
     'measure','prob');
-[~, badchans.spec] = pop_rejchan(EEG, ...
+[~, badchans.spec_bad, badchans.spec_measure] = pop_rejchan(EEG, ...
     'elec',chan_inds, ...
     'threshold',opts.thresh, ...
     'norm','on', ...
@@ -102,27 +109,42 @@ badchans.given = opts.badchans;
 
 %Find the bad channels
 chan_inds = find( ismember( {EEG.chanlocs.type}, 'EEG' ) );
-badchans.bad_inds = unique( struct2array( badchans ) );
+badchans.bad_inds = unique( [badchans.prob_bad badchans.spec_bad badchans.given] );
 badchans.bad_labels = {EEG.chanlocs(badchans.bad_inds).labels};
 fprintf('\n\n%d channel(s) marked as bad from automated detection:\n', length(badchans.bad_inds) );
 fprintf('\t%d channels marked manually prior to this function\n', length(badchans.given));
-fprintf('\t%d channels marked as improbable (%d threshold)\n', length(badchans.prob), opts.thresh);
-fprintf('\t%d channels marked based on bad spectrum (%d threshold)\n\n', length(badchans.spec), opts.thresh);
-pause( 5 ); % Pause for 5 seconds
-    
-%% Plot the bad channels
-% Determine channel color (bad in red; good is black)
-colors = cell(1,length(chan_inds)); colors(:) = { 'k' };
-colors(badchans.bad_inds) = { 'r' };
+fprintf('\t%d channels marked as improbable (%d threshold)\n', length(badchans.prob_bad), opts.thresh);
+fprintf('\t%d channels marked based on bad spectrum (%d threshold)\n\n', length(badchans.spec_bad), opts.thresh);
+pause( 3 ); % Pause for 5 seconds
+
+%% Plot the data
+% Butterfly plot
+butterfly_f = figure('Units','Normalized');
+butterfly_f.Position(1:2) = [.2 .5];
+pop_timtopo(EEG, [EEG.xmin EEG.xmax] * 1000, [100 200 300 linspace(500,EEG.xmax*1000,4)], 'Butterfly Plot');
+
+% Save figure
+if ~isempty(opts.fig_dir)
+    saveas(butterfly_f,fullfile(opts.fig_dir,'mark_bad_chans_butterfly.png'));
+end    
 
 % Plot channel properties
-spec_f = figure;
+spec_f = figure('Units','Normalized');
+spec_f.Position(1:2) = [.5 .5];
 EEG_spect = pop_select(EEG, 'channel',chan_inds);
 pop_spectopo(EEG_spect, 1, [EEG.xmin EEG.xmax]*EEG.srate, 'EEG' , ...
-    'percent', 15, ...
+    'percent', 25, ...
     'freq', [3 10 15 25 40 55 80], ...
     'freqrange', opts.freq_range, ...
     'electrodes','off');
+% Save figure
+if ~isempty(opts.fig_dir)
+    saveas(spec_f,fullfile(opts.fig_dir,'mark_bad_chans_freq_spectrum.png'));
+end    
+
+% Determine channel color (bad in red; good is black)
+colors = cell(1,length(chan_inds)); colors(:) = { 'k' };
+colors(badchans.bad_inds) = { 'r' };
 
 % Show the plot
 eegplot(EEG.data(chan_inds,:,:), 'srate', EEG.srate, 'title', sprintf('%s: Look through data - Bad Channels Marked in Red', num2str(EEG.subject)), ...
@@ -131,6 +153,7 @@ eegplot(EEG.data(chan_inds,:,:), 'srate', EEG.srate, 'title', sprintf('%s: Look 
 plot_f = gcf;
 waitfor( plot_f);
 try close(spec_f); end
+try close(butterfly_f); end
 
 %% Ask if any channels you want to manually label as bad
 man_rej = questdlg('Do you want (or need) to manually reject any channels?');
@@ -184,6 +207,49 @@ if ~isempty(badchans.bad_inds)
 else
     
     fprintf('No channels were marked as bad!!!\n');
+    
+end
+
+%% Plot a table of the values
+% Make a table, and save as an image
+if ~isempty(opts.fig_dir)
+    
+    % Setup the figure
+    f = figure('Visible','off','Units','Normalized','Color','white','Position',[.4 .1 .4 .7]);
+        
+    % Make the table
+    table = repmat({''},EEG.nbchan, 4);
+    table(badchans.bad_inds,1) = {'bad'};
+    table(badchans.given,2)    = {true};
+    table(:,3)                 = num2cell(badchans.prob_measure);
+    table(:,4)                 = num2cell(badchans.spec_measure);
+    
+    % Get the rownames
+    rownames    = {EEG.chanlocs.labels};
+    columnnames = {'Is Bad?' 'Manual' 'Prob. Measure' 'Spec. Measure'};
+    
+    % Reshape table
+    table1 = table(1:32,:);
+    table2 = table(33:end,:);
+    rownames1 = rownames(1:32);
+    rownames2 = rownames(33:end);
+    
+    % Write the table1
+    uitable('Parent',f,'Data',table1,'Rowname',rownames1,'Units','Normalized', ...
+        'Position',[.05 .02 .4 .9],'ColumnName',columnnames,'ColumnWidth',{50 50 80 80}, ...
+        'FontSize',8,'RowStriping','on');
+    
+    % Write the table2
+    uitable('Parent',f,'Data',table2,'Rowname',rownames2,'Units','Normalized', ...
+        'Position',[.55 .02 .4 .9],'ColumnName',columnnames,'ColumnWidth',{50 50 80 80}, ...
+        'FontSize',8,'RowStriping','on');
+    
+    uicontrol('Parent',f,'Style','text','String','Bad Channel Output', ...
+        'Units','Normalized','Position',[.3 .92 .4 .05], 'BackgroundColor','white', ...
+        'FontWeight','bold');
+    
+    % Save figure
+    saveas(f,fullfile(opts.fig_dir,'mark_bad_chans_table.png'));
     
 end
 
