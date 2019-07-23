5046b8014838bc688fdc0012fdb492cb368bd8b5
jdkoen <koen.joshua@gmail.com>
2019-07-23 15:47:11 -0400

    update message handling

--- MATLAB/general/make_dirs.m
+++ MATLAB/general/make_dirs.m
@@ -3,8 +3,12 @@ function make_dirs(directory_list)
 % Make directories
 fprintf('MAKING DIRECTORIES:\n')
 for directory = string(directory_list)
-    if ~isfolder(directory), mkdir(directory); end
-    fprintf('\tCREATED:  %s\r', directory)
+    if ~isfolder(directory) 
+        mkdir(directory);
+        fprintf('\tCREATED:  %s\n', directory); 
+    else
+        fprintf('\tEXISTS:   %s\n', directory);
+    end
 end
 
 end % of function
