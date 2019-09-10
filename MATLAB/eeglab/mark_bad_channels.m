e2ad44831c58140825e15e205a587e8c9aa2d6ff
Morgan Widhalm <mwidhalm@nd.edu>
2019-09-10 15:48:53 -0400

    add function to relevant directory for use
    add function to relevant directory for use

--- MATLAB/eeglab/mark_bad_channels.m
+++ MATLAB/eeglab/mark_bad_channels.m
@@ -15,12 +15,9 @@
 %                   Must contain the following fields:
 %                   1) 'elec' - [n1 n2 ...] electrode number(s) to take into 
 %                   consideration for rejection
-%                   2) 'thresh' - [max] absolute threshold or activity probability 
-%                   limit(s) (in std. dev.)
-%                   3) 'freq_range' - range of frequencies in the spectral
-%                   decomposision.
-%                   4) 'badchans'   - vector of any bad channels indicies
+%                   2) 'badchans'   - vector of any bad channels indicies
 %                                     from other approaches.
+%                   3) 'plot_freq_spect' - option to plot or not
 %
 % Outputs:
 %   EEG           - EEG set with new field (EEG.bad_channels). Contains
@@ -51,12 +48,6 @@ function EEG = mark_bad_channels(EEG, opts)
 % Input fields
 input_fields = fieldnames(opts);
 
-% % Make sure required fields are present
-% req_fields = {'thresh' 'freq_range'};
-% if ~all( ismember( req_fields, input_fields ) )
-%     error('options requires thresh and freq_range.')
-% end
-
 % Make sure to have either chan_type or chan_inds
 chan_fields = {'chan_type' 'chan_inds'};
 if ~any( ismember( chan_fields, input_fields) )
@@ -70,10 +61,10 @@ if ~isfield(opts, 'badchans')
     opts.badchans = [];
 end
 
-% % Handle fig_dir
-% if ~isfield(opts,'fig_dir')
-%     opts.fig_dir = '';
-% end
+% Handle fig_dir
+if ~isfield(opts,'fig_dir')
+    opts.fig_dir = '';
+end
 
 %% Select channels for analysis (and the field in opts)
 % Determine which channels to process
@@ -94,75 +85,72 @@ badchans = struct();
 % Copy from opts.badchans
 badchans.given = opts.badchans;
 
-% % Run automated bad channel detection w/ probability and spectrum
-% [~, badchans.prob_bad, badchans.prob_measure] = pop_rejchan(EEG, ...
-%     'elec', chan_inds, ...
-%     'threshold',opts.thresh, ...
-%     'norm','on', ...
-%     'measure','prob');
-% [~, badchans.spec_bad, badchans.spec_measure] = pop_rejchan(EEG, ...
-%     'elec',chan_inds, ...
-%     'threshold',opts.thresh, ...
-%     'norm','on', ...
-%     'measure','spec', ...
-%     'freqrange', opts.freq_range);
-
 %Find the bad channels
 chan_inds = find( ismember( {EEG.chanlocs.type}, 'EEG' ) );
-%badchans.bad_inds = unique( [badchans.prob_bad badchans.spec_bad badchans.given] );
 badchans.bad_inds = unique( [badchans.given] );
 badchans.bad_labels = {EEG.chanlocs(badchans.bad_inds).labels};
-% fprintf('\n\n%d channel(s) marked as bad from automated detection:\n', length(badchans.bad_inds) );
 fprintf('\t%d channels marked manually prior to this function\n', length(badchans.given));
-% fprintf('\t%d channels marked as improbable (%d threshold)\n', length(badchans.prob_bad), opts.thresh);
-% fprintf('\t%d channels marked based on bad spectrum (%d threshold)\n\n', length(badchans.spec_bad), opts.thresh);
-pause( 3 ); % Pause for 5 seconds
+pause( 3 ); % Pause for 3 seconds
 
 %% Plot the data
-% Butterfly plot
-butterfly_f = figure('Units','Normalized');
-butterfly_f.Position(1:2) = [.2 .5];
-pop_timtopo(EEG, [EEG.xmin EEG.xmax] * 1000, [100 200 300 linspace(500,EEG.xmax*1000,4)], 'Butterfly Plot');
 
-% Save figure
-if ~isempty(opts.fig_dir)
-    saveas(butterfly_f,fullfile(opts.fig_dir,'mark_bad_chans_butterfly.png'));
-end    
+%Plot frequency spectrum, only if input is 'yes'
+if strcmp(opts.freq_spect, 'yes')
+    
+    % Plot channel properties
+    spec_f = figure('Units','Normalized');
+    spec_f.Position(1:2) = [.5 .5];
+    EEG_spect = pop_select(EEG, 'channel',chan_inds);
+    pop_spectopo(EEG_spect, 1, [EEG.xmin EEG.xmax]*EEG.srate, 'EEG' , ...
+        'percent', 25, ...
+        'freq', [3 10 15 25 40 55 80], ...
+        'freqrange', [.1 100], ...
+        'electrodes','off');
+    
+    % Save figure
+    if ~isempty(opts.fig_dir)
+        saveas(spec_f,fullfile(opts.fig_dir,'mark_bad_chans_freq_spectrum.png'));
+    end
+    
+    % Wait for user to close plot before continuing
+    plot_f = gcf;
+    waitfor( plot_f);
+    
+end %end of if loop to plot frequency spectrum
 
-% Plot channel properties
-spec_f = figure('Units','Normalized');
-spec_f.Position(1:2) = [.5 .5];
-EEG_spect = pop_select(EEG, 'channel',chan_inds);
-pop_spectopo(EEG_spect, 1, [EEG.xmin EEG.xmax]*EEG.srate, 'EEG' , ...
-    'percent', 25, ...
-    'freq', [3 10 15 25 40 55 80], ...
-    'freqrange', opts.freq_range, ...
-    'electrodes','off');
+%Plot channel scroll, only if input is 'yes'
 
-% Save figure
-if ~isempty(opts.fig_dir)
-    saveas(spec_f,fullfile(opts.fig_dir,'mark_bad_chans_freq_spectrum.png'));
-end    
+if strcmp(opts.plot_EEG_scroll, 'yes')
+    
+    % Determine channel color (bad in red; good is black)
+    colors = cell(1,length(chan_inds)); colors(:) = { 'k' };
+    colors(badchans.bad_inds) = { 'r' };
+    
+    % Show the plot
+    eegplot(EEG.data(chan_inds,:,:), 'srate', EEG.srate, 'title', sprintf('%s: Look through data - Bad Channels Marked in Red', num2str(EEG.subject)), ...
+        'limits', [EEG.xmin EEG.xmax]*1000, 'color', colors, 'eloc_file', EEG.chanlocs(chan_inds), ...
+        'events',EEG.event, 'command', []);
+    
+    % Wait for user to close plot before continuing
+    plot_f = gcf;
+    waitfor( plot_f);
+    
+end %end of if loop to plot EEG scroll to manually inspect channels
 
-% Determine channel color (bad in red; good is black)
-colors = cell(1,length(chan_inds)); colors(:) = { 'k' };
-colors(badchans.bad_inds) = { 'r' };
-
-% Show the plot
-eegplot(EEG.data(chan_inds,:,:), 'srate', EEG.srate, 'title', sprintf('%s: Look through data - Bad Channels Marked in Red', num2str(EEG.subject)), ...
-    'limits', [EEG.xmin EEG.xmax]*1000, 'color', colors, 'eloc_file', EEG.chanlocs(chan_inds), ...
-    'events',EEG.event, 'command', []);
-plot_f = gcf;
-waitfor( plot_f);
 try close(spec_f); end
-try close(butterfly_f); end
 
 %% Ask if any channels you want to manually label as bad
 man_rej = questdlg('Do you want (or need) to manually reject any channels?');
 if strcmpi(man_rej,'yes')
     
+    %Loop through the channel indices and make a cell array where the
+    %format is 'channel # - channel name' to be input into listdlg
+    for chani = 1:length(chan_inds)
+        liststring{chani,:} = sprintf('%d-%s', chan_inds(chani), EEG.chanlocs(chan_inds(chani)).labels);
+    end
+    
     badchans.manual = listdlg( ...
-        'ListString', {EEG.chanlocs(chan_inds).labels}, ...
+        'ListString', liststring, ...
         'SelectionMode', 'multiple', ...
         'Name', sprintf('%s: Manual Channel Rejection', num2str(EEG.subject)), ...
         'PromptString', 'Select Channels for Manual Rejection (press cancel if none)', ...
@@ -178,27 +166,27 @@ end
 badchans.bad_inds   =  unique(horzcat(badchans.bad_inds, badchans.manual));
 badchans.bad_labels = {EEG.chanlocs(badchans.bad_inds).labels};
 
-% %% Ask if there are any channels you want to remove that are marked as bad
-% if ~isempty(badchans.bad_inds)
-% 
-%     clear_bads = questdlg('Do you want to unmark a channel labeled as bad (i.e., mark it as good)?');
-%     if strcmpi(clear_bads,'yes')
-%         bad_marked_good = listdlg( ...
-%             'ListString', badchans.bad_labels, ...
-%             'SelectionMode', 'multiple', ...
-%             'Name', sprintf('%s: Channels marked as bad', num2str(EEG.subject)), ...
-%             'PromptString', 'Select Bad Channels to set Status to Good (press cancel if all should be bad)', ...
-%             'ListSize', [600 300] );
-%     else
-%         bad_marked_good = [];
-%     end
-%     
-%     % Update
-%     badchans.bad_inds(bad_marked_good) = [];
-%     badchans.bad_marked_good = badchans.bad_labels(bad_marked_good);
-%     badchans.bad_labels = {EEG.chanlocs(badchans.bad_inds).labels};
-%     
-% end
+%% Ask if there are any channels you want to remove that are marked as bad
+if ~isempty(badchans.bad_inds)
+
+    clear_bads = questdlg('Do you want to unmark a channel labeled as bad (i.e., mark it as good)?');
+    if strcmpi(clear_bads,'yes')
+        bad_marked_good = listdlg( ...
+            'ListString', badchans.bad_labels, ...
+            'SelectionMode', 'multiple', ...
+            'Name', sprintf('%s: Channels marked as bad', num2str(EEG.subject)), ...
+            'PromptString', 'Select Bad Channels to set Status to Good (press cancel if all should be bad)', ...
+            'ListSize', [600 300] );
+    else
+        bad_marked_good = [];
+    end
+    
+    % Update
+    badchans.bad_inds(bad_marked_good) = [];
+    badchans.bad_marked_good = badchans.bad_labels(bad_marked_good);
+    badchans.bad_labels = {EEG.chanlocs(badchans.bad_inds).labels};
+    
+end
     
 %% Output info
 if ~isempty(badchans.bad_inds)
@@ -211,49 +199,6 @@ else
     fprintf('No channels were marked as bad!!!\n');
     
 end
-
-% %% Plot a table of the values
-% % Make a table, and save as an image
-% if ~isempty(opts.fig_dir)
-%     
-%     % Setup the figure
-%     f = figure('Visible','off','Units','Normalized','Color','white','Position',[.4 .1 .4 .7]);
-%         
-%     % Make the table
-%     table = repmat({''},EEG.nbchan, 4);
-%     table(badchans.bad_inds,1) = {'bad'};
-%     table(badchans.given,2)    = {true};
-%     table(:,3)                 = num2cell(badchans.prob_measure);
-%     table(:,4)                 = num2cell(badchans.spec_measure);
-%     
-%     % Get the rownames
-%     rownames    = {EEG.chanlocs.labels};
-%     columnnames = {'Is Bad?' 'Manual' 'Prob. Measure' 'Spec. Measure'};
-%     
-%     % Reshape table
-%     table1 = table(1:32,:);
-%     table2 = table(33:end,:);
-%     rownames1 = rownames(1:32);
-%     rownames2 = rownames(33:end);
-%     
-%     % Write the table1
-%     uitable('Parent',f,'Data',table1,'Rowname',rownames1,'Units','Normalized', ...
-%         'Position',[.05 .02 .4 .9],'ColumnName',columnnames,'ColumnWidth',{50 50 80 80}, ...
-%         'FontSize',8,'RowStriping','on');
-%     
-%     % Write the table2
-%     uitable('Parent',f,'Data',table2,'Rowname',rownames2,'Units','Normalized', ...
-%         'Position',[.55 .02 .4 .9],'ColumnName',columnnames,'ColumnWidth',{50 50 80 80}, ...
-%         'FontSize',8,'RowStriping','on');
-%     
-%     uicontrol('Parent',f,'Style','text','String','Bad Channel Output', ...
-%         'Units','Normalized','Position',[.3 .92 .4 .05], 'BackgroundColor','white', ...
-%         'FontWeight','bold');
-%     
-%     % Save figure
-%     saveas(f,fullfile(opts.fig_dir,'mark_bad_chans_table.png'));
-%     
-% end
 
 %% Return EEG
 EEG.etc.bad_channels = badchans;
---
+++ MATLAB/eeglab/pop_manual_artifact_flag.m
@@ -0,0 +1,177 @@
+% pop_manual_artifact_flag() -  This function plots the data with the currently
+%                      marked artifacts, and allows the user to manually
+%                      mark artifacts. Before plotting, a prompt asks for a
+%                      artifact flag to mark the ERPLAB EVENTLIST.
+% Usage:
+%   >>  [EEG, com] = pop_manual_artifact_flag( EEG, elecRange, artFlag ) 
+%
+% Inputs:
+%   EEG        - current dataset structure or structure array
+%   elecRange   - electrodes/channels to test for linear drift
+%   artFlag     - (optional) the artifact flag to assign epochs with linear
+%                 drift (requires ERPLAB)
+%    
+% Outputs:
+%   EEG        - current dataset structure or structure array
+%
+% See also:
+%    POP_EEGPLOT, EEGPLOT
+
+% Copyright (C) 2015  Joshua D. Koen
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+function [EEG, com] = pop_manual_artifact_flag( EEG, elecRange, artFlag ) 
+
+% the command output is a hidden output that does not have to
+% be described in the header
+
+com = ''; % this initialization ensure that the function will return something
+          % if the user press the cancel button            
+
+%% display help if not enough arguments
+% ------------------------------------
+if nargin < 1
+	help pop_manual_artifact_flag;
+	return;
+end
+
+%% pop up window
+% -------------
+if nargin < 2
+    
+    % Set up the options and default values
+    commandchans = [ 'tmpchans = get(gcbf, ''userdata'');' ...
+                     'tmpchans = tmpchans{1};' ...
+                     'set(findobj(gcbf, ''tag'', ''chantype''), ''string'', ' ...
+                     '       int2str(pop_chansel( tmpchans )));' ...
+                     'clear tmpchans;' ];
+    commandtype = ['tmptype = get(gcbf, ''userdata'');' ...
+                   'tmptype = tmptype{2};' ...
+                   'if ~isempty(tmptype),' ...
+                   '    [tmps,tmpv, tmpstr] = listdlg2(''PromptString'',''Select type(s)'', ''ListString'', tmptype);' ...
+				   '    if tmpv' ...
+				   '        set(findobj(''parent'', gcbf, ''tag'', ''chantype''), ''string'', tmpstr);' ...
+				   '    end;' ...
+                   'else,' ...
+                   '    warndlg2(''No channel type'', ''No channel type'');' ...
+                   'end;' ...
+				   'clear tmps tmpv tmpstr tmptype tmpchans;' ];
+               
+    % Define the prompts
+	promptstr    = { ...
+        { 'style' 'text' 'string' 'Channel type(s) or indices' }, ...
+        { 'style' 'edit' 'string' '' 'tag' 'chantype' }, ...
+        { 'style' 'pushbutton' 'string' '... types' 'callback' commandtype } ...
+        { 'style' 'pushbutton' 'string' '... channels' 'callback' commandchans }, ...
+        { 'style' 'text' 'string' 'Artifact Flag (for ERPLAB; 2 to 8)' }, ...
+        { 'style' 'edit' 'string' '2' }, ...
+        };
+    
+    % Define geomery
+    geometry = { [2 1 1 1] [2 1.5] };
+    
+    % channel types
+    % -------------
+    if isfield(EEG.chanlocs, 'type')
+        tmpchanlocs = EEG(1).chanlocs;
+        alltypes = { tmpchanlocs.type };
+        indempty = cellfun('isempty', alltypes);
+        alltypes(indempty) = '';
+        try
+            alltypes = unique_bc(alltypes);
+        catch
+            alltypes = '';
+        end
+    else
+        alltypes = '';
+    end
+    
+    % channel labels
+    % --------------
+    if ~isempty(EEG.chanlocs)
+        tmpchanlocs = EEG(1).chanlocs;        
+        alllabels = { tmpchanlocs.labels };
+    else
+        for index = 1:EEG(1).nbchan
+            alllabels{index} = int2str(index);
+        end
+    end
+    
+    % Make GUI
+    result = inputgui('geometry',geometry,'uilist',promptstr, ...
+        'helpcom', 'pophelp(''pop_detectdrift'')', ...
+        'title', 'Manual Artifact Detection == pop_manual_artifact_flag()', ...
+        'userdata', { alllabels alltypes } );
+    
+    % Sort the results into the appropriate variables, and check input for
+    % errors
+    % Electrodes
+    if ~isempty(result{1})
+        if ~isempty(str2num(result{1})), elecRange = str2num(result{1});
+        else                             elecRange = parsetxt(result{1}); 
+        end
+    end
+    
+% elseif nargin == 3
+%     
+%     error('Must provide all inputs.')
+    
+end
+    
+%% Chech if ERPLAB installed and EVENTLIST created
+% ------------------------------------
+if exist(which('eegplugin_erplab'),'file')
+   if ~isfield(EEG,'EVENTLIST')
+       error('Must create EVENTLIST structure.')
+   end
+else
+    error('Must have the ERPLAB plugin installed.')
+end
+
+%% Get the current artifact flags (if any), and make the format for eegplot()
+% ------------------------------------
+% INITIALIZE EEG>REJECT>REJMANUAL FOR INSTANCES WHEN IT IS EMPTY (SET TO
+% zeros(1,size(EEG.data,3)); Same for EEG.reject.rejmanualE
+if EEG.reject.rejmanual == []
+    EEG.reject.rejmanual = zeros(1,size(EEG.data,3));
+end
+if EEG.reject.rejmanualE == []
+    EEG.reject.rejmanualE = zeros(1,size(EEG.data,3));
+end
+
+artEpochs = find(EEG.reject.rejmanual);
+if ~isempty(artEpochs) % Make winrej structure
+    winrej = trial2eegplot(EEG.reject.rejmanual,EEG.reject.rejmanualE(elecRange,:,:),EEG.pnts,[ 0.7 1 0.9]);
+else
+    winrej = [];
+end
+
+%% Plot the EEG data with eegplot()
+% ------------------------------------
+cmd = [ 'artEpochs = find(EEG.reject.rejmanual);' ...
+    'EEG.reject.rejmanual = eegplot2trial( TMPREJ, EEG.pnts, EEG.trials);'  ];
+butlabel = 'UPDATE MARKS';
+eegplot(EEG.data(elecRange,:,:),'eloc_file',EEG.chanlocs(elecRange),'srate',EEG.srate, ...
+    'command',cmd,'butlabel',butlabel,'wincolor',[ 0.7 1 0.9], ...
+    'winrej',winrej,'events',EEG.event);
+waitfor( findobj('parent', gcf, 'string', butlabel), 'userdata');
+
+%% return the string command for detect drift
+% -------------------------
+com = [ com sprintf('%s = pop_manual_artifact_flag(%s);', inputname(1), ...
+		inputname(1))]; 
+
+end
